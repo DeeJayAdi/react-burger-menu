57223afc9f30c1206d966c54af0ce80a5cddd01c
DeeJayAdi <deejayadipl@gmail.com>
2022-08-30 21:51:11 +0200

    Create desktop view

--- src/App.js
+++ src/App.js
@@ -34,9 +34,11 @@ class App extends Component {
       ] || this.props.buttons,
   };
   showMenu = () => {
-    this.setState((prev) => ({
-      settings: { ...prev.settings, display: !prev.settings.display },
-    }));
+    if (isMobile) {
+      this.setState((prev) => ({
+        settings: { ...prev.settings, display: !prev.settings.display },
+      }));
+    }
   };
   componentDidMount() {
     if (!isMobile) {
@@ -57,11 +59,11 @@ class App extends Component {
           classNames="show"
           unmountOnExit
         >
-          <>
+          <div className="react-hamburger-menu-list">
             {this.state.buttons.map((bt, index) => (
               <Button key={index} data={bt} showMenu={this.showMenu} />
             ))}
-          </>
+          </div>
         </CSSTransition>
       </div>
     );
